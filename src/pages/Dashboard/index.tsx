55651e28f2facfd4ae2fab18594b5323354508b3
Gustavo Lopes <38574162+gustavolopesv3@users.noreply.github.com>
2020-11-23 17:24:00 -0500

    input and button

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
+import logoImg from '../../assets/logo.svg';
 const Dashboard: React.FC = () => {
-  return <Title>Explore Repositorios no gitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore Repositorios no gitHub</Title>
+      <Form>
+        <input type="text" placeholder="Digite o nome do repositorio" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories></Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
