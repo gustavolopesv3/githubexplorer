b0c3d87c96fed0a237d9a43728cd8c249723b572
Gustavo Lopes <38574162+gustavolopesv3@users.noreply.github.com>
2020-11-23 17:53:49 -0500

    cods repositories

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Title, Form, Repositories } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 const Dashboard: React.FC = () => {
   return (
@@ -10,7 +11,41 @@ const Dashboard: React.FC = () => {
         <input type="text" placeholder="Digite o nome do repositorio" />
         <button type="submit">Pesquisar</button>
       </Form>
-      <Repositories></Repositories>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/38574162?s=460&u=214063de7a21a67c111c02296c8e4eb9ab251584&v=4"
+            alt="Gustavo lopes"
+          />
+          <div>
+            <strong>gustavolopesv3/myprofile</strong>
+            <p>my professional profile and portfolio</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/38574162?s=460&u=214063de7a21a67c111c02296c8e4eb9ab251584&v=4"
+            alt="Gustavo lopes"
+          />
+          <div>
+            <strong>gustavolopesv3/myprofile</strong>
+            <p>my professional profile and portfolio</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/38574162?s=460&u=214063de7a21a67c111c02296c8e4eb9ab251584&v=4"
+            alt="Gustavo lopes"
+          />
+          <div>
+            <strong>gustavolopesv3/myprofile</strong>
+            <p>my professional profile and portfolio</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
