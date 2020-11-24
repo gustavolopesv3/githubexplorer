8ccfa9798ed74f684615656d5e519e624dfd7c3c
Gustavo Lopes <38574162+gustavolopesv3@users.noreply.github.com>
2020-11-23 21:39:46 -0500

    alter fild estatic para dinamic

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -72,11 +72,11 @@ const Repository: React.FC = () => {
               <span>Stars</span>
             </li>
             <li>
-              <strong>22</strong>
+              <strong>{repository.forks_count}</strong>
               <span>Forks</span>
             </li>
             <li>
-              <strong>451</strong>
+              <strong>{repository.open_issues_count}</strong>
               <span>Issues abertas</span>
             </li>
           </ul>
